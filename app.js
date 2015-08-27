5bb8aba73d3a4fc39b2c32ec7204cf3c3348d047
bgrove1974 <bgroveben@gmail.com>
2015-08-27 02:19:22 -0700

    Basic Routing

--- app.js
+++ app.js
@@ -10,6 +10,30 @@ var users = require('./routes/users');
 
 var app = express();
 
+
+/* For more info on routing, go to http://expressjs.com/guide/routing.html */
+/* The following code illustrates some example routes in an app. */
+
+// respond with "Hello World!" on the homepage:
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+    res.send('Got a POST request');
+});
+
+// accept PUT request at /user
+app.put('/user', function (req, res) {
+    res.send('Got a PUT request at /user');
+});
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+    res.send('Got a DELETE request at /user');
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
